420f0b4805c31937de945ddad96f32fcaab46c69
Nancy-Khaled <131464596+Nancy-Khaled@users.noreply.github.com>
2023-11-01 14:48:21 +0200

    create sign in and sign up pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import {FaUser} from "react-icons/fa";
+import {MdEmail} from "react-icons/md";
+import {MdPassword} from "react-icons/md";
+
+import '../assets/css/sign-in.css'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <section>
+        <div className="container">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div className="card mt-5 mb-5">
+                <div className="card-body p-5">
+                  <h2 className="text-uppercase text-center mb-5">
+                    Sign In
+                  </h2>
+
+                  <form method='' action=''>
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <FaUser></FaUser>
+                    </span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Name"
+                      />
+                    </div>
+
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <MdEmail></MdEmail>
+                    </span>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter Email"
+                      />
+                    </div>
+
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <MdPassword></MdPassword>
+                    </span>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter Password"
+                      />
+                    </div>
+
+                    <div className="d-flex justify-content-center">
+                      <Link
+                        type="button" to={"/profile"}
+                        className="btn btn-outline-secondary"
+                      >
+                        Login
+                      </Link>
+                    </div>
+
+                    <p className="text-center text-muted mt-5 mb-0">
+                      Have already an account?{" "}
+                      <Link to="/sign-up" className="fw-bold">
+                        <u>Sign Up</u>
+                      </Link>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {FaUser} from "react-icons/fa";
+import {MdEmail} from "react-icons/md";
+import {MdPassword} from "react-icons/md";
+
+import '../assets/css/sign-up.css'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <section>
+        <div className="container">
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div className="card mt-5 mb-5">
+                <div className="card-body p-5">
+                  <h2 className="text-uppercase text-center mb-5">
+                    Sign Up
+                  </h2>
+
+                  <form>
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <FaUser></FaUser>
+                    </span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Name"
+                      />
+                    </div>
+
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <MdEmail></MdEmail>
+                    </span>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter Email"
+                      />
+                    </div>
+
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <MdPassword></MdPassword>
+                    </span>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter Password"
+                      />
+                    </div>
+
+                    <div className="form-item mb-4">
+                    <span className="icon">
+                      <MdPassword></MdPassword>
+                    </span>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Repeat password"
+                      />
+                    </div>
+
+                    <div className="d-flex justify-content-center">
+                      <Link
+                        type="button" to={"/profile"}
+                        className="btn btn-outline-secondary"
+                      >
+                        Register
+                      </Link>
+                    </div>
+
+                    <p className="text-center text-muted mt-5 mb-0">
+                      Have already an account?{" "}
+                      <Link to="/sign-in" className="fw-bold">
+                        <u>SignIn</u>
+                      </Link>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  );
 }
